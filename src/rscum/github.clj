aea00378f0ac9b5f901a88a8dd434ecff167cb45
Ben Ashford <ben.ashford@xbridge.com>
2012-06-20 17:04:37 +0100

    Fetch all pages of results from GitHub API

--- src/rscum/github.clj
+++ src/rscum/github.clj
@@ -2,8 +2,19 @@
   (:use [tentacles.users :only [following]])
   (:use [tentacles.repos :only [watching]]))
 
+(def +per-page+ 100)
+
+(defn- fetch-all [f & params]
+  (loop [results []
+         page 1]
+    (let [next-results (apply #(f % {:page page :per-page +per-page+}) params)
+          concat-results (concat results next-results)]
+      (if (< (count next-results) +per-page+)
+        concat-results
+        (recur concat-results (inc page))))))
+
 (defn following-users [username]
-  (map :login (following username)))
+  (map :login (fetch-all following username)))
 
 (defn watching-repos [username]
-  (map :full_name (watching username)))
+  (map :full_name (fetch-all watching username)))
