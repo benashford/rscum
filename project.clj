5bfea639d224d22e0d89e2e4cead09163cba2554
Ben Ashford <ben.ashford@xbridge.com>
2012-06-26 13:17:54 +0100

    Added incanter dependency

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [org.clojars.tavisrudd/redis-clojure "1.3.1"]
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
-                 [net.sf.jung/jung-algorithms "2.0.1"]]
+                 [net.sf.jung/jung-algorithms "2.0.1"]
+                 [incanter "1.3.0"]]
   :main rscum.core
   :jvm-opts ["-Xmx1g"])
