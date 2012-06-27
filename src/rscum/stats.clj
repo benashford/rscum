4634c12e0ea3028bd028be26eacf7feb1921cebd
Ben Ashford <ben.ashford@xbridge.com>
2012-06-27 15:23:21 +0100

    Moved utility functions to util namespace

--- src/rscum/stats.clj
+++ src/rscum/stats.clj
@@ -1,23 +1,6 @@
 (ns rscum.stats
-  (:use [clojure.set]))
-
-;; Move to util package
-(defn pair-map
-  "Apply a map function on each pair of users"
-  [map-f users]
-  (letfn [(outer-f [users]
-    (let [user (first users)]
-      (when-let [other-users (next users)]
-        (letfn [(inner-f [inner-users]
-          (if (empty? inner-users)
-            (outer-f other-users)
-            (let [other-inner-users (rest inner-users)
-                  v (map-f user (first inner-users))]
-              (if-not (nil? v)
-                (lazy-seq (cons v (inner-f other-inner-users)))
-                (recur other-inner-users)))))]
-          (inner-f other-users)))))]
-    (outer-f users)))
+  (:use [clojure.set])
+  (:use [rscum.util]))
 
 ;;
 ;; Similarity scoring
@@ -68,13 +51,6 @@
 
 (defn rand-double-seq [lower upper]
   (lazy-seq (cons (rand-double lower upper) (rand-double-seq lower upper))))
-
-(defn zip [& xs]
-  (when (first (first xs))
-    (lazy-seq
-      (cons
-        (map first xs)
-        (apply zip (map next xs))))))
 
 ;;
 ;; Dimension reduceing
---
+++ src/rscum/util.clj
@@ -0,0 +1,25 @@
+(ns rscum.util)
+
+(defn pair-map
+  "Apply a map function on each pair of users"
+  [map-f users]
+  (letfn [(outer-f [users]
+    (let [user (first users)]
+      (when-let [other-users (next users)]
+        (letfn [(inner-f [inner-users]
+          (if (empty? inner-users)
+            (outer-f other-users)
+            (let [other-inner-users (rest inner-users)
+                  v (map-f user (first inner-users))]
+              (if-not (nil? v)
+                (lazy-seq (cons v (inner-f other-inner-users)))
+                (recur other-inner-users)))))]
+          (inner-f other-users)))))]
+    (outer-f users)))
+
+(defn zip [& xs]
+  (when (first (first xs))
+    (lazy-seq
+      (cons
+        (map first xs)
+        (apply zip (map next xs))))))
