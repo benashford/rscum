bf68027a9e344978f1ae511a4b0a38184c15c8f2
Ben Ashford <ben@bcash.org>
2012-08-18 14:17:13 +0100

    Removed Mongo dependencies (not needed in this project)

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
                  [net.sf.jung/jung-algorithms "2.0.1"]
-                 [incanter "1.3.0"]]
+                 [incanter "1.3.0" :exclusions [org.mongodb/mongo-java-driver]]]
   :main rscum.core
   :plugins [[lein-swank "1.4.4"]]
   :jvm-opts ["-Xmx1g"])
