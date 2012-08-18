c93c95297e80bb8d87ca11532fb10405d2040767
Ben Ashford <ben@bcash.org>
2012-08-18 18:12:46 +0100

    Sleeping if GitHub API quota is expired

--- src/rscum/github.clj
+++ src/rscum/github.clj
@@ -5,11 +5,32 @@
 
 (def +per-page+ 100)
 
+(defn rate-limit
+  "Temporary until a newer version of Tentacles is published on Clojars"
+  []
+  ((api-call :get "rate_limit" nil nil) :rate))
+
+(let [rl (rate-limit)]
+  (def +limit+ (rl :limit))
+  (def remaining (atom (rl :remaining))))
+
+(defn- wait-for-quota
+  []
+  (println "Quota limit reached, sleeping for one minute...")
+  (Thread/sleep 60000)
+  (println "...re-checking quota...")
+  (swap! remaining + ((rate-limit) :remaining))
+  (println "...remaining: " @remaining)
+  (if (<= @remaining 0) (recur)))
+
 (defn- fetch-all [f & params]
   (loop [results []
          page 1]
+    (if (<= @remaining 0)
+      (wait-for-quota))
     (let [next-results (apply #(f % {:page page :per-page +per-page+}) params)
           concat-results (concat results next-results)]
+      (swap! remaining dec)
       (if (< (count next-results) +per-page+)
         concat-results
         (recur concat-results (inc page))))))
@@ -20,8 +41,3 @@
 
 (fromgithub following-users [username] (map :login) (following username))
 (fromgithub watching-repos [username] (map :full_name) (watching username))
-
-;; Temporary until a newer version of Tentacles is published on Clojars
-
-(defn rate-limit []
-  (api-call :get "rate_limit" nil nil))
