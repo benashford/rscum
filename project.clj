0b315db513afaa819c870dba52788a1b91940642
Ben Ashford <ben@bcash.org>
2012-08-18 18:27:35 +0100

    Newest version of Tentacles

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject rscum "1.0.0-SNAPSHOT"
   :description "TBC"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [tentacles "0.1.3"]
+                 [tentacles "0.2.0-beta1"]
                  [org.clojars.tavisrudd/redis-clojure "1.3.1"]
                  [net.sf.jung/jung-api "2.0.1"]
                  [net.sf.jung/jung-graph-impl "2.0.1"]
